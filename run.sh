8f5599ba4cfdc53516eb78d236d84c27e68ebccd
cecilpang <cpang2@binghamton.edu>
2026-03-13 00:03:56 -0400

    Test viz-1d-data, viz-2d-data and viz-1d-multi-method workflows via run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 # uv sync
 uv run python workflow/viz_1d_data.py
 uv run python workflow/viz_2d_data.py
-# uv run python workflow/viz_1d_multi_method.py
+uv run python workflow/viz_1d_multi_method.py
 # uv run python workflow/viz_digits.py
