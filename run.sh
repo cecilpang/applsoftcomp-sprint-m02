053a65ad934fdcd17095a367a940bb7b8cbe5a37
cecilpang <cpang2@binghamton.edu>
2026-03-13 14:22:06 -0400

    Test four workflows via run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 uv run python workflow/viz_1d_data.py
 uv run python workflow/viz_2d_data.py
 uv run python workflow/viz_1d_multi_method.py
-# uv run python workflow/viz_digits.py
+uv run python workflow/viz_digits.py
